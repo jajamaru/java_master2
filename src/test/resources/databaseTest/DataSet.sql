bcaf8aa8225e0679178559e9a7f0b2f5e902a876
romain huret <romain.boulot62@gmail.com>
2015-03-06 13:07:34 +0100

    BD - Mise à jour des scripts

--- src/test/resources/databaseTest/DataSet.sql
+++ src/test/resources/databaseTest/DataSet.sql
@@ -1,10 +1,10 @@
 ALTER TABLE `PERSON` AUTO_INCREMENT = 1;
-INSERT INTO `PERSON` (name, birthday) VALUES 
-	('p1', '2000-01-01'),
-	('p2', '2001-01-01'),
-	('p3', '2002-01-01'),
-	('p4', '2003-01-01'),
-	('p5', '2004-01-01')
+INSERT INTO `PERSON` (name, birthday,sexe) VALUES 
+	('p1', '2000-01-01', 'H'),
+	('p2', '2001-01-01', 'F'),
+	('p3', '2002-01-01', 'H'),
+	('p4', '2003-01-01', 'F'),
+	('p5', '2004-01-01', 'H')
 ;
 
 ALTER TABLE `FRIEND` AUTO_INCREMENT = 1;
