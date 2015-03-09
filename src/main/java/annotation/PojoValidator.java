cd334f8a5ef544c8bc3c280d13597aa6a8d3ace1
romain huret <romain.boulot62@gmail.com>
2015-03-09 17:47:54 +0100

    SONAR CORRECTION

--- src/main/java/annotation/PojoValidator.java
+++ src/main/java/annotation/PojoValidator.java
@@ -47,13 +47,18 @@ public class PojoValidator implements ConstraintValidator<Pojo, Object> {
     }
 
     final Class<?> cl = object.getClass();
+    final Set<Method> gettersAndSetters = getGettersAndSetters(cl.getDeclaredMethods());
 
     final boolean validCompleteName = isOnlyClass(cl) && isValidName(cl.getSimpleName())
         && isValidPackageName(cl);
     final boolean validDoWithEntity = doOnlyWithEntity(cl);
+    final boolean validAttributes = allPrivateAttributs(cl.getDeclaredFields());
+    final boolean validAllAttributes = gettersAndSettersForAll(cl.getDeclaredFields(),
+        gettersAndSetters);
+    final boolean validPublicGetter = gettersAndSettersArePublic(gettersAndSetters);
     final boolean validProcessing = noProcessing(cl.getDeclaredMethods());
 
-    if (!validAttributeProperties(cl, cv)) {
+    if (!validAttributes || !validAllAttributes || !validPublicGetter) {
       addErrorMessageProperties("{pojo.message.error.attribut.private}", cv);
       addErrorMessageProperties("{pojo.message.error.attribut.getter}", cv);
       addErrorMessageProperties("{pojo.message.error.getter.public}", cv);
@@ -71,15 +76,6 @@ public class PojoValidator implements ConstraintValidator<Pojo, Object> {
     }
 
     return validCompleteName && validDoWithEntity && validProcessing;
-  }
-
-  private boolean validAttributeProperties(final Class<?> cl, final ConstraintValidatorContext cv) {
-    final Set<Method> gettersAndSetters = getGettersAndSetters(cl.getDeclaredMethods());
-    final boolean validAttributes = allPrivateAttributs(cl.getDeclaredFields());
-    final boolean validAllAttributes = gettersAndSettersForAll(cl.getDeclaredFields(),
-        gettersAndSetters);
-    final boolean validPublicGetter = gettersAndSettersArePublic(gettersAndSetters);
-    return validAttributes && validAllAttributes && validPublicGetter;
   }
 
   private void addErrorMessageProperties(final String message, final ConstraintValidatorContext cv) {
